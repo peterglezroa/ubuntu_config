575c785f8d17d99dc9e455993ac23291b86bcfaa
peterglezroa <peterglezroa@gmail.com>
2021-11-23 12:54:05 -0600

    Add command to update wal

--- resources/build.sh
+++ resources/build.sh
@@ -7,5 +7,11 @@ ln -s ./images/wallpapers $HOME/Pictures/wallpapers
 # /home/peterglezroa/.local/bin/wal --backend colorthief -i /home/peterglezroa/Pictures/wallpapers/
 # /home/peterglezroa/.local/bin/wal --backend colorthief -i /home/peterglezroa/Pictures/wallpapers/ -o /home/peterglezroa/bin/openrgb_pywal
 
+# Run wal on init
+# touch ~/.xsessionrc
+#echo 'wal -i "$(< "${HOME}/.cache/wal/wal")" --backend colorthief' >> ~/.xsessionrc
+
+# Wal on bashrc
+# echo 'cat ~/.cache/wal/sequences' >> ~/.bashrc
 # Hide sidebar
 # Appearance -> Behavior -> Auto Hide Launcher
