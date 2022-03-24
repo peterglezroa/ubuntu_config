f3a2b3dd103dda6ca76d385e27881896935d07f8
peterglezroa <peterglezroa@gmail.com>
2022-03-24 13:30:25 -0600

    update alacritty

--- themes/build.sh
+++ themes/build.sh
@@ -1,7 +1,22 @@
 apt install gnome-tweaks gtk2-engines-murrine gnome-themes-extra sassc
-cd $HOME/downloads/tmp
-git clone git@github.com:vinceliuice/Orchis-theme.git
-cd Orchis-theme
+
+# Colloid theme
+cd $HOME/builds/
+git clone git@github.com:vinceliuice/Colloid-gtk-theme.git
+cd Colloid-gtk-theme
 ./install.sh
 
-apt install gnome-shell-extension-autohidetopbar
+# Colloid icons
+#cd $HOME/builds/
+#git clone git@github.com:vinceliuice/Colloid-icon-theme.git
+#cd Colloid-icon-theme
+#./install.sh
+
+
+# https://extensions.gnome.org/extension/307/dash-to-dock/
+
+# Rounded corners
+cd $HOME/builds/
+git clone git@github.com:owozsh/adwaita-rounded.git
+cd adwaita-rounded
+./install.sh
